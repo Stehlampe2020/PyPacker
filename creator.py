87f9ad0ddcf6e4d64c48eab872428fa79a149ff4
Stehlampe2020 <Stehlampe2020@gmail.com>
2023-06-08 21:08:19 +0200

    Add TODO to creator.py Rename packer.py→u_n_packer.py

--- creator.py
+++ creator.py
@@ -6,4 +6,13 @@ Dateien in 7z-Archiv einfügen, evtl. mit Passwortschutz und auf jeden fall in 1
 die dann in b-Strings eingelagert werden um direkt auf die Festplatte geschrieben und wieder entpackt werden zu können.
 """
 
-import packer
+import gzip #TODO: l2db3 fertig machen, hier reinkopieren
+
+def compress(data:bytes) -> bytes:
+    """GZip-compresses `data`."""
+    return gzip.compress(data)
+
+def decompress(data:bytes) -> bytes:
+    """GZip-decompresses `data`."""
+    return gzip.decompress(data)
+
