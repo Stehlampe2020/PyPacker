0745a1632003a44c8d0bbbdc5950e96b965e5334
Stehlampe2020 <Stehlampe2020@gmail.com>
2023-06-08 20:21:32 +0200

    Add a lot of files * Add PyChar project structure * Add two of the later three .py files

---
+++ creator.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Dateien in 7z-Archiv einfügen, evtl. mit Passwortschutz und auf jeden fall in 100mb-Teile zerteilt,
+die dann in b-Strings eingelagert werden um direkt auf die Festplatte geschrieben und wieder entpackt werden zu können.
+"""
+
+import packer
